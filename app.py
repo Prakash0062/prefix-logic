16e1e5634a45ea516652bea491e6170386c2ea42
FRUSTATED Engineer <149618147+Prakash0062@users.noreply.github.com>
2025-05-31 01:48:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def process():
                            audio_file=f'audio/{audio_filename}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
